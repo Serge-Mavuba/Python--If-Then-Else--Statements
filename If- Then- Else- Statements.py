6e7683aa81b403aebe4b04e233feeb7f83a71371
Serge-Mavuba <79016443+Serge-Mavuba@users.noreply.github.com>
2021-04-02 01:28:01 -0400

    Update If- Then- Else- Statements.py

--- If- Then- Else- Statements.py
+++ If- Then- Else- Statements.py
@@ -4,8 +4,12 @@ import colorama
 from colorama import Fore, Back, Style
 colorama.init(autoreset=True)
 
-input = input("Please enter your password: ")
+# input function will promt user to enter a string and store the value in the variable input
+input = input("Please enter your password: " + Fore.WHITE + Back.WHITE + Style.BRIGHT + "")
 
+# using if-then line, test to see if the length of the string is less thasn 6 characters
+# if true the following lines are executed
 if len(input) < 6:
-    print ("Your password " + Fore.WHITE + Back.WHITE + Style.BRIGHT + str(input) + Fore.WHITE+ Back.BLACK + Style.BRIGHT + " is" + Fore.RED+ Back.BLACK + Style.BRIGHT + " too short.")
+    print (Fore.WHITE+ Back.BLACK + Style.BRIGHT + "Your password " + Fore.WHITE + Back.WHITE + Style.BRIGHT + str(input) + Fore.WHITE+ Back.BLACK + Style.BRIGHT + " is" + Fore.RED+ Back.BLACK + Style.BRIGHT + " too short.")
     print("Please enter a password with at least" + Fore.RED + Style.BRIGHT + " 6 characters.")
+# if not the lines are skipped
